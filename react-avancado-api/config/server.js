ac3aac4293947fadcc83250535b32744f185e4f0
david <davidwdiniz@gmail.com>
2021-01-02 10:25:02 -0300

    Strapi data structure from landing page CMS

---
+++ react-avancado-api/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '7fa68c666661411424b9e427d953d4b7'),
+    },
+  },
+});
---
+++ react-avancado-api/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '955e5edc-b925-4c22-9b16-ba1acb0f3494'
+};
