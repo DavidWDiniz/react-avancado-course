5e35ee8c8474a70f4acae93bab68566629fa5d05
DavidWDiniz <davidwdiniz@gmail.com>
2021-01-16 19:46:34 -0300

    Starting won games strapi api

---
+++ won-games-api/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '1368c58737ea486d6bda34a747706cc8'),
+    },
+  },
+});
---
+++ won-games-api/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '7fc4b028-4f87-4507-a03f-eaaf2279d7dd'
+};
