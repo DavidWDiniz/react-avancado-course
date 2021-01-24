14dc12efb72eb1d2a79b0dc80d214ab20734dca9
DavidWDiniz <davidwdiniz@gmail.com>
2021-01-24 10:53:31 -0300

    Customization in left menu

---
+++ won-games-api/admin/src/components/LeftMenu/LeftMenuFooter/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Wrapper from './Wrapper';
+
+function LeftMenuFooter() {
+  return (
+    <Wrapper>
+      <div className="poweredBy">
+        <a key="website" href="https://github.com/DavidWDiniz" target="_blank" rel="noopener noreferrer">
+          David William Diniz
+        </a>
+      </div>
+    </Wrapper>
+  );
+}
+
+export default LeftMenuFooter;
