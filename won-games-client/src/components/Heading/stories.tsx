52a8ee92523632eb17257f5e3a3b9fe45cbb9c06
DavidWDiniz <davidwdiniz@gmail.com>
2021-01-30 10:55:00 -0300

    Add Heading component

---
+++ won-games-client/src/components/Heading/stories.tsx
@@ -0,0 +1,20 @@
+import { Story, Meta } from '@storybook/react';
+import Heading, { HeadingProps } from '.';
+
+export default {
+  title: 'Heading',
+  component: Heading,
+  argTypes: {
+    children: {
+      type: 'string',
+    },
+  },
+} as Meta;
+
+export const Default: Story<HeadingProps> = (args) => (
+  <Heading {...args}>{args.children}</Heading>
+);
+
+Default.args = {
+  children: 'Most Popular',
+};
