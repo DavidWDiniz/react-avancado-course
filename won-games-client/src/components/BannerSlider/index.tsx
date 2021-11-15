64e9006aa6055a84b1ac006dea90b5c31c325d48
David <davidwdiniz@gmail.com>
2021-11-15 13:41:19 -0300

    Add BannerSlider component

---
+++ won-games-client/src/components/BannerSlider/index.tsx
@@ -0,0 +1,37 @@
+import * as S from './styles';
+import Banner, { BannerProps } from '../Banner';
+import Slider from '../Slider';
+import { Settings } from 'react-slick';
+
+export type BannerSliderProps = {
+  items: BannerProps[];
+};
+
+const settings: Settings = {
+  dots: true,
+  arrows: false,
+  vertical: true,
+  verticalSwiping: true,
+  infinite: false,
+  responsive: [
+    {
+      breakpoint: 1170,
+      settings: {
+        vertical: false,
+        verticalSwiping: false,
+      },
+    },
+  ],
+};
+
+const BannerSlider = ({ items }: BannerSliderProps) => (
+  <S.Wrapper>
+    <Slider settings={settings}>
+      {items.map((item) => (
+        <Banner key={item.title} {...item} />
+      ))}
+    </Slider>
+  </S.Wrapper>
+);
+
+export default BannerSlider;
